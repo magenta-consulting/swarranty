e99963318d92a47f1751fb9edf4a57e9cc78f1a3
Peter Pierre <binh@sunrise.vn>
2018-06-20 12:05:00 +0700

    /** 	 * @Route("/bean/playground", name="bean_playground") 	 */

--- src/Controller/BeanPlaygroundController.php
+++ src/Controller/BeanPlaygroundController.php
@@ -8,10 +8,17 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Media\Media;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BeanPlaygroundController extends Controller {
+	/**
+	 * @Route("/", name="home")
+	 */
+	public function home() {
+		return new Response('hello kitty');
+	}
 	
 	/**
 	 * @Route("/bean/playground", name="bean_playground")
@@ -27,11 +34,11 @@ class BeanPlaygroundController extends Controller {
 //		   ->join('c.partOf', 'partOf');;
 //		$chapter = $qb->setFirstResult(0)->getQuery()->getResult();
 		$manager = $this->get('doctrine.orm.default_entity_manager');
-		$m = new Media();
+		$m       = new Media();
 		$m->setName('File Name');
 		$m->setEnabled(true);
 		$m->setProviderName('sonata.media.provider.image');
-		
+
 //		$test = $this->container->get('sonata.media.controller.api.gallery');
 		
 		return $this->render('bean_playground/index.html.twig', [
