cbfa7835ee551d775ddfaffdcbb70b00fa7bedaa
Peter Pierre <binh@sunrise.vn>
2018-07-03 10:19:32 +0700

    - warranty nullable

--- libraries/bundle/MagentaSWarrantyModelBundle/src/Entity/Customer/Warranty.php
+++ libraries/bundle/MagentaSWarrantyModelBundle/src/Entity/Customer/Warranty.php
@@ -370,7 +370,7 @@ class Warranty extends FullTextSearch implements ThingChildInterface, DecisionMa
 	
 	/**
 	 * @var string|null
-	 * @ORM\Column(type="string")
+	 * @ORM\Column(type="string",nullable=true)
 	 */
 	protected
 		$dealerName;
