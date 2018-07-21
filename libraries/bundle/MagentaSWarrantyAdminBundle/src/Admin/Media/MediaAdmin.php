e042c2c0222fd38951aa8e116d6fa2f06f7663aa
Peter Pierre <binh@sunrise.vn>
2018-07-21 19:25:07 +0700

    return parent::isGranted($name, $object);

--- libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Media/MediaAdmin.php
+++ libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Media/MediaAdmin.php
@@ -7,11 +7,11 @@ use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserService;
 use Sonata\MediaBundle\Admin\ORM\MediaAdmin as SonataMediaAdmin;
 
 class MediaAdmin extends SonataMediaAdmin {
-//	public function isGranted($name, $object = null) {
-//		if($name === 'VIEW') {
-//			return parent::isGranted($name, $object);
-//		} else {
-//			return $this->getConfigurationPool()->getContainer()->get(UserService::class)->getUser()->isAdmin();
-//		}
-//	}
+	public function isGranted($name, $object = null) {
+		if($name === 'CREATE') {
+			return true;
+		} else {
+			return parent::isGranted($name, $object);
+		}
+	}
 }
