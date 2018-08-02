b431823bd1e0298dd367e9d301465d89564d0422
Peter Pierre <binh@sunrise.vn>
2018-08-02 15:46:21 +0700

    $criteria

--- libraries/bundle/MagentaSWarrantyApiBundle/src/Controller/Product/BrandCategoryController.php
+++ libraries/bundle/MagentaSWarrantyApiBundle/src/Controller/Product/BrandCategoryController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BrandCategoryController extends Controller {
 	public function productsAction(BrandCategory $data): Collection {
-		$prods = $data->getProducts();
+		$prods = $data->getEnabledProducts();
 		
 		return $prods;
 	}
