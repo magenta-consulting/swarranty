c381d416d4291f3d45bc7266f10dfbcc53da0e09
nguyenhongphat0 <hongphat.js@gmail.com>
2018-08-07 14:14:05 +0700

    change production api

--- libraries/spa/ngx-reg/src/environments/environment.prod.ts
+++ libraries/spa/ngx-reg/src/environments/environment.prod.ts
@@ -2,11 +2,10 @@ export const environment = {
   production: true
 };
 
-// export const apiEndPoint = 'http://127.0.0.1:8000';
-export const apiEndPoint = 'http://dev-swarranty.magentapulse.com';
-export const apiEndPointBase = '/api';
+export const apiEndPoint = 'http://swarranty-api.magentapulse.com';
+export const apiEndPointBase = '';
 export const organisationPath = '/organisations';
-export const MEDIA_PREFIX = '/media';
-export const apiEndPointMedia   = 'http://dev-swarranty.magentapulse.com/media-api';
+export const MEDIA_PREFIX = '';
+export const apiEndPointMedia   = 'http://swarranty-media.magentapulse.com';
 export const apiMediaUploadPath = '/providers/sonata.media.provider.image/media.json';
 export const binariesMedia = '/binaries/reference/view.json';
