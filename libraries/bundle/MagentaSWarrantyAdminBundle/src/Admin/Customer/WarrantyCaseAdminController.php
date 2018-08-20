9fb699a126084cb9b8e1791dd4e279cd24a4320e
Peter Pierre <binh@sunrise.vn>
2018-08-20 14:19:58 +0700

    Xlsx Export for WarrantyCase

--- libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Customer/WarrantyCaseAdminController.php
+++ libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Customer/WarrantyCaseAdminController.php
@@ -3,9 +3,14 @@
 namespace Magenta\Bundle\SWarrantyAdminBundle\Admin\Customer;
 
 use Magenta\Bundle\SWarrantyAdminBundle\Admin\BaseCRUDAdminController;
+use Magenta\Bundle\SWarrantyModelBundle\Entity\Customer\Warranty;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Customer\WarrantyCase;
+use Magenta\Bundle\SWarrantyModelBundle\Entity\Product\Brand;
+use Magenta\Bundle\SWarrantyModelBundle\Entity\Product\Product;
+use Magenta\Bundle\SWarrantyModelBundle\Entity\Product\ServiceZone;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\System\DecisionMakingInterface;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\User\User;
+use Magenta\Bundle\SWarrantyModelBundle\Service\SpreadsheetWriter;
 use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserService;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -16,6 +21,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyPath;
@@ -68,6 +74,9 @@ class WarrantyCaseAdminController extends BaseCRUDAdminController {
 			);
 		}
 		
+		if($format === 'xls') {
+			$format = 'xlsx';
+		}
 		$filename = sprintf(
 			'export_%s_%s.%s',
 			strtolower(substr($this->admin->getClass(), strripos($this->admin->getClass(), '\\') + 1)),
@@ -89,6 +98,123 @@ class WarrantyCaseAdminController extends BaseCRUDAdminController {
 				'cases' => $cases
 			], RouterInterface::ABSOLUTE_URL);
 			$response       = new RedirectResponse($c->getParameter('PDF_API_BASE_URL') . $c->getParameter('PDF_DOWNLOAD_PREFIX') . str_replace('?', '%3F', $htmlDisplayUrl) . '/' . 'service-sheets_' . $d->format('d-m-Y'));
+			
+			return $response;
+		} else if($format === 'xlsx') {
+			// ask the service for a Excel5
+			$phpExcelObject = new Spreadsheet();
+			
+			$phpExcelObject->getProperties()->setCreator("Solution")
+			               ->setLastModifiedBy("Solution")
+			               ->setTitle("Download - Raw Data")
+			               ->setSubject("Order Item - Raw Data")
+			               ->setDescription("Raw Data")
+			               ->setKeywords("office 2005 openxml php")
+			               ->setCategory("Raw Data Download");
+			
+			$phpExcelObject->setActiveSheetIndex(0);
+			$activeSheet = $phpExcelObject->getActiveSheet();
+			
+			$activeSheet
+				->setCellValue('A1', "ID")
+				->setCellValue('B1', "Number")
+				->setCellValue('C1', "Priority")
+				->setCellValue('D1', "Date Created")
+				->setCellValue('E1', "Date Closed")
+				->setCellValue('F1', "Product Brand")
+				->setCellValue('G1', "Product Category")
+				->setCellValue('H1', "Product Model Name")
+				->setCellValue('I1', "Case Description")
+				->setCellValue('J1', "Service Notes")
+				->setCellValue('K1', "Special Notes")
+				->setCellValue('L1', "Technicians")
+				->setCellValue('M1', "Service Zones")
+				->setCellValue('N1', "Customer Name")//			            ->setCellValue('O1', "Technician Name(s)")
+			;
+			
+			// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+			$phpExcelObject->setActiveSheetIndex(0);
+			$activeSheet = $phpExcelObject->getActiveSheet();
+			$sWriter     = new SpreadsheetWriter($activeSheet);
+			$sWriter->goFirstColumn();
+			$sWriter->goFirstRow();
+			
+			$source = $this->admin->getDataSourceIterator();
+			$repo   = $this->getDoctrine()->getRepository(WarrantyCase::class);
+			foreach($source as $data) {
+				$case = $repo->find($data['id']);
+				$sWriter->goDown();
+				$sWriter->goFirstColumn();
+				
+				$sWriter->writeCellAndGoRight($case->getId());
+				// Number
+				$sWriter->writeCellAndGoRight($case->getNumber());
+				$sWriter->writeCellAndGoRight($case->getPriority());
+				$sWriter->writeCellAndGoRight($case->getCreatedAt()->format('d-m-Y'));
+				$closedAtStr = '';
+				if( ! empty($case->getClosedAt())) {
+					$closedAtStr = $case->getClosedAt()->format('d-m-Y');
+				}
+				$sWriter->writeCellAndGoRight($closedAtStr);
+				$brand        = '';
+				$category     = '';
+				$modelName    = '';
+				$zoneStr      = '';
+				$customerName = '';
+				/** @var Warranty $w */
+				if( ! empty($w = $case->getWarranty())) {
+					/** @var Product $p */
+					if( ! empty($p = $w->getProduct())) {
+						/** @var Brand $b */
+						if( ! empty($b = $p->getBrand())) {
+							$brand = $b->getName();
+						}
+						if( ! empty($c = $p->getCategory())) {
+							$category = $c->getName();
+						}
+						$modelName = $p->getName();
+					}
+					
+					if( ! empty($customer = $w->getCustomer())) {
+						$customerName = $customer->getName();
+					}
+				}
+				
+				/** @var ServiceZone $zone */
+				if( ! empty($zone = $case->getServiceZone())) {
+					$zoneStr = $zone->getName();
+				}
+				
+				$sWriter->writeCellAndGoRight($brand);
+				$sWriter->writeCellAndGoRight($category);
+				$sWriter->writeCellAndGoRight($modelName);
+				$sWriter->writeCellAndGoRight($case->getDescription());
+				$sWriter->writeCellAndGoRight($case->getServiceNotesString());
+				$sWriter->writeCellAndGoRight($case->getSpecialRemarks());
+				$sWriter->writeCellAndGoRight($case->getAssigneeString());
+				$sWriter->writeCellAndGoRight($zoneStr);
+				$sWriter->writeCellAndGoRight($customerName);
+				
+			}
+			
+			// create the writer
+			$writer = new Xlsx($phpExcelObject);
+//			$writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel5');
+			// create the response
+//			$response = $this->get('phpexcel')->createStreamedResponse($writer);
+			$response = new StreamedResponse(
+				function() use ($writer) {
+					$writer->save('php://output');
+				},
+				200,
+				[]
+			);
+			
+			$response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
+			$response->headers->set('Pragma', 'public');
+			$response->headers->set('Cache-Control', 'maxage=1');
+			
+			$response->headers->set('Content-Disposition', 'attachment;filename=' . $filename);
 			
 			return $response;
 		}
