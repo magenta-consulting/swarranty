48683d43df2a4f4cd186aea85d5e87a30c255e85
binhle410 <binh@sunrise.vn>
2019-08-01 13:11:14 +0800

    pdf upload of the image

---
+++ README.md
@@ -0,0 +1,3 @@
+Dependencies
+- ghostscript
+- imagick
--- libraries/bundle/MagentaSWarrantyModelBundle/src/SonataMedia/Resizer/SimpleResizer.php
+++ libraries/bundle/MagentaSWarrantyModelBundle/src/SonataMedia/Resizer/SimpleResizer.php
@@ -14,14 +14,15 @@ class SimpleResizer extends \Sonata\MediaBundle\Resizer\SimpleResizer
         if ('application/pdf' === $contentType) {
             // create Imagick object
             $imagick = new \Imagick();
+            // /workspace/magenta/swarranty/dev/libraries/bundle/MagentaSWarrantyModelBundle/src/SonataMedia/Resizer/
+//            file_put_contents('file1.pdf', $content);
             // Reads image from PDF
-            $imagick->readImageBlob($content);
-            $imagick->setImageFormat("jpg");
+            $imagick->readImage('pdf.png');
+            $imagick->setImageFormat('png');
 
             $time = date('d-m-Y');
             // Writes an image or image sequence Example- converted-0.jpg, converted-1.jpg
 //            $imagick->writeImages($time.'.jpg', true);
-
 
             // copy form parent::resize
             if (!isset($settings['width'])) {
@@ -31,11 +32,23 @@ class SimpleResizer extends \Sonata\MediaBundle\Resizer\SimpleResizer
             $imageContent = $imagick->getImagesBlob();
             $image = $this->adapter->load($imageContent);
 
+//            var_dump($settings);            die();
+//            if (empty($settings['width'])) {
+//                $settings['width'] = $settings['height'];
+//            } else {
+//                $settings['height'] = $settings['width'];
+//            }
+            $media->setWidth(300);
+            $media->setHeight(514);
+            $box = $this->getBox($media, $settings);
+//            var_dump($box);            die();
             $content = $image
-                ->thumbnail($this->getBox($media, $settings), $this->mode)
-                ->get($format, ['quality' => $settings['quality']]);
-
-            $out->setContent($content, $this->metadata->get($media, $out->getName()));
+                ->thumbnail($box, $this->mode)
+                ->get('png', ['quality' => $settings['quality']]);
+            $metadata = $this->metadata->get($media, $out->getName());
+//            var_dump($metadata);            exit();
+            $metadata['contentType'] = 'image/png';
+            $out->setContent($content, $metadata);
         } else {
             parent::resize($media, $in, $out, $format, $settings);
         }
