a1a63e04502c953106777e8a689496a1e135e308
binhle410 <binh@sunrise.vn>
2019-08-06 11:35:36 +0800

    pdf fix

--- libraries/bundle/MagentaSWarrantyAdminBundle/src/Twig/MagentaTwigExtension.php
+++ libraries/bundle/MagentaSWarrantyAdminBundle/src/Twig/MagentaTwigExtension.php
@@ -2,7 +2,6 @@
 
 namespace Magenta\Bundle\SWarrantyAdminBundle\Twig;
 
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Media\Media;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Organisation\Organisation;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Organisation\OrganisationMember;
 use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserService;
@@ -10,55 +9,60 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
-class MagentaTwigExtension extends AbstractExtension {
-	
-	const TYPE_PDF_DOWNLOAD_SERVICE_SHEET = 'PDF_DOWNLOAD_SERVICE_SHEET';
-	
-	/** @var ContainerInterface $container */
-	private $container;
-	
-	public function __construct(ContainerInterface $c) {
-		$this->container = $c;
-	}
-	
-	public function getFilters() {
-		return array(
-			new TwigFilter('privateMediumUrl', array( $this, 'privateMediumUrl' )),
-		);
-	}
-	
-	public function getFunctions() {
-		return array(
-			new \Twig_SimpleFunction('currentOrganisation', array( $this, 'getCurrentOrganisation' )),
-			new \Twig_SimpleFunction('organisationBySubdomain', array( $this, 'organisationBySubdomain' )),
-			new \Twig_SimpleFunction('privateMediumUrl', array( $this, 'privateMediumUrl' )),
-		);
-	}
-	
-	public function getCurrentOrganisation() {
-		$repo = $this->container->get('doctrine')->getRepository(Organisation::class);
-		$user = $this->container->get(UserService::class)->getUser();
-		if(empty($org = $user->getAdminOrganisation())) {
-			if( ! empty($person = $user->getPerson())) {
-				/** @var OrganisationMember $m */
-				$m = $person->getMembers()->first();
-				if( ! empty($m)) {
-					return $m->getOrganization();
-				}
-			}
-		}
-		
-		return $org;
-	}
-	
-	public function downloadPdfUrl($type) {
-//		if($type === )
-	
-	}
-	
-	public function privateMediumUrl($mediumId, $format = 'admin') {
-		$c = $this->container;
-		
-		return $c->get('sonata.media.manager.media')->generatePrivateUrl($mediumId, $format);
-	}
+class MagentaTwigExtension extends AbstractExtension
+{
+    const TYPE_PDF_DOWNLOAD_SERVICE_SHEET = 'PDF_DOWNLOAD_SERVICE_SHEET';
+
+    /** @var ContainerInterface $container */
+    private $container;
+
+    public function __construct(ContainerInterface $c)
+    {
+        $this->container = $c;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('privateMediumUrl', [$this, 'privateMediumUrl']),
+        ];
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('currentOrganisation', [$this, 'getCurrentOrganisation']),
+            new \Twig_SimpleFunction('organisationBySubdomain', [$this, 'organisationBySubdomain']),
+            new \Twig_SimpleFunction('privateMediumUrl', [$this, 'privateMediumUrl']),
+        ];
+    }
+
+    public function getCurrentOrganisation()
+    {
+        $repo = $this->container->get('doctrine')->getRepository(Organisation::class);
+        $user = $this->container->get(UserService::class)->getUser();
+        if (empty($org = $user->getAdminOrganisation())) {
+            if (!empty($person = $user->getPerson())) {
+                /** @var OrganisationMember $m */
+                $m = $person->getMembers()->first();
+                if (!empty($m)) {
+                    return $m->getOrganization();
+                }
+            }
+        }
+
+        return $org;
+    }
+
+    public function downloadPdfUrl($type)
+    {
+        //		if($type === )
+    }
+
+    public function privateMediumUrl($mediumId, $format = 'admin')
+    {
+        $c = $this->container;
+
+        return $c->get('sonata.media.manager.media')->generatePrivateUrl($mediumId, $format);
+    }
 }
