779e5c8c445ad04f394eabd925830f51b2793698
binhle410 <binh@sunrise.vn>
2019-10-15 13:44:54 +0700

    <div class="row">             <div>               <b><u>Customer Acknowledgement</u></b><br>               I confirm that the above work/services has been completed to my satisfaction and acknowledge the following conditions :             </div>             <div style="margin-top:10px;">               (i) Fujioh Int'l Tdg Pte Ltd provides one (1) month warranty on service and three(3) months on parts replaced.</div>             <div style="margin-top:5px;">               (ii) Fujioh Int'l Tdg Pte Ltd reserves all rights to change the above terms and conditions without prior notice.</div>           </div>

--- libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Customer/CustomerAdmin.php
+++ libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Customer/CustomerAdmin.php
@@ -2,79 +2,59 @@
 
 namespace Magenta\Bundle\SWarrantyAdminBundle\Admin\Customer;
 
-use Doctrine\ORM\EntityRepository;
-use Magenta\Bundle\SWarrantyAdminBundle\Admin\AccessControl\ACLAdmin;
 use Magenta\Bundle\SWarrantyAdminBundle\Admin\BaseAdmin;
-use Magenta\Bundle\SWarrantyAdminBundle\Form\Type\ManyToManyThingType;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Customer\Customer;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Customer\Warranty;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Organisation\Organisation;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Person\Person;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Product\ServiceZone;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\User\User;
-use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserService;
-use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder;
-use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserManager;
-use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserManagerInterface;
-use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
-use Sonata\CoreBundle\Form\Type\DatePickerType;
 use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery;
-use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
-use Sonata\MediaBundle\Form\Type\MediaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CustomerAdmin extends BaseAdmin
 {
-    
     const CHILDREN = [
         WarrantyAdmin::class => 'customer',
     ];
-    
-    
+
     protected $action;
-    
-    protected $datagridValues = array(
+
+    protected $datagridValues = [
         // display the first page (default = 1)
 //        '_page' => 1,
         // reverse order (default = 'ASC')
         '_sort_order' => 'DESC',
         // name of the ordered field (default = the model's id field, if any)
         '_sort_by' => 'updatedAt',
-    );
-    
+    ];
+
     public function getNewInstance()
     {
         /** @var Customer $object */
         $object = parent::getNewInstance();
-        
+
         return $object;
     }
-    
+
     /**
-     * @param string $name
+     * @param string   $name
      * @param Customer $object
      */
     public function isGranted($name, $object = null)
     {
         return parent::isGranted($name, $object);
     }
-    
+
     public function toString($object)
     {
         return $object instanceof Customer
             ? $object->getName()
             : 'Customer'; // shown in the breadcrumb on the create view
     }
-    
+
     public function createQuery($context = 'list')
     {
         /** @var ProxyQuery $query */
@@ -86,55 +66,54 @@ class CustomerAdmin extends BaseAdmin
         $qb = $query->getQueryBuilder();
         $expr = $qb->expr();
         $rootAlias = $qb->getRootAliases()[0];
-        $qb->andWhere($expr->eq($rootAlias . '.enabled', ':trueValue'))
+        $qb->andWhere($expr->eq($rootAlias.'.enabled', ':trueValue'))
             ->setParameter('trueValue', true);
 
 //        $query->andWhere()
-        
+
         return $query;
     }
-    
+
     public function getPersistentParameters()
     {
         $parameters = parent::getPersistentParameters();
         if (!$this->hasRequest()) {
             return $parameters;
         }
-        
+
         $request = $this->getRequest();
-        
-        if ($request->get('workflow') === 'transfer-ownership') {
+
+        if ('transfer-ownership' === $request->get('workflow')) {
             if (!empty($wid = $request->get('warranty'))) {
                 $parameters = array_merge($parameters, [
                     'workflow' => 'transfer-ownership',
-                    'warranty' => $wid
+                    'warranty' => $wid,
                 ]);
             }
         }
-        
+
         if (empty($org = $this->getCurrentOrganisation(false))) {
             return $parameters;
         }
-        
-        return array_merge($parameters, array(
-            'organisation' => $org->getId()
-        ));
+
+        return array_merge($parameters, [
+            'organisation' => $org->getId(),
+        ]);
     }
-    
+
     public function configureRoutes(RouteCollection $collection)
     {
         parent::configureRoutes($collection);
-//		$collection->add('show_user_profile', $this->getRouterIdParameter() . '/show-user-profile');
-    
+        //		$collection->add('show_user_profile', $this->getRouterIdParameter() . '/show-user-profile');
     }
-    
+
     public function getTemplate($name)
     {
         $_name = strtoupper($name);
-        
+
         return parent::getTemplate($name);
     }
-    
+
     protected function configureShowFields(ShowMapper $showMapper)
     {
         $showMapper
@@ -150,12 +129,12 @@ class CustomerAdmin extends BaseAdmin
             ->with('form_group.warranty_records', ['class' => 'col-md-6'])
             ->add('warranties', null, [
                 'label' => false,
-                'associated_property' => 'product.name'
+                'associated_property' => 'product.name',
 //				'template'            => '@MagentaSWarrantyAdmin/CRUD/Association/show_one_to_many.html.twig'
             ])
             ->end();
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -163,27 +142,27 @@ class CustomerAdmin extends BaseAdmin
     {
         $actions = [];
         $request = $this->getRequest();
-        if ($request->get('workflow') === 'transfer-ownership') {
+        if ('transfer-ownership' === $request->get('workflow')) {
             $actions = ['transfer_warranty_ownership' => ['template' => '@MagentaSWarrantyAdmin/Admin/Customer/Customer/Action/list__action__transfer_warranty_ownership.html.twig']];
         }
-        $actions = array_merge($actions, array(
-            'show' => array(),
-            'edit' => array(),
-            'delete' => array(),
+        $actions = array_merge($actions, [
+            'show' => [],
+            'edit' => [],
+            'delete' => [],
 //					'send_evoucher' => array( 'template' => '::admin/employer/employee/list__action_send_evoucher.html.twig' )
 
 //                ,
 //                    'view_description' => array('template' => '::admin/product/description.html.twig')
 //                ,
 //                    'view_tos' => array('template' => '::admin/product/tos.html.twig')
-        ));
-        
+        ]);
+
         $listMapper->add('_action', 'actions', [
                 'actions' => $actions,
-                'label' => 'form.label_action'
+                'label' => 'form.label_action',
             ]
         );
-        
+
         $listMapper
             ->add('name', null, ['editable' => true, 'label' => 'form.label_name'])
             ->add('email', null, ['editable' => true, 'label' => 'form.label_email'])
@@ -193,9 +172,9 @@ class CustomerAdmin extends BaseAdmin
             ->add('subscribedToNewsletter', 'boolean', ['label' => 'form.label_subscribed'])
             ->add('enabled', null, ['editable' => true, 'label' => 'form.label_enabled']);
 
-//		$listMapper->add('positions', null, [ 'template' => '::admin/user/list__field_positions.html.twig' ]);
+        //		$listMapper->add('positions', null, [ 'template' => '::admin/user/list__field_positions.html.twig' ]);
     }
-    
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
@@ -213,7 +192,7 @@ class CustomerAdmin extends BaseAdmin
             ->add('enabled', null, ['label' => 'form.label_enabled']);
         $formMapper->end();
     }
-    
+
     /**
      * @param Customer $object
      */
@@ -224,7 +203,7 @@ class CustomerAdmin extends BaseAdmin
             $object->setEnabled(true);
         }
     }
-    
+
     /**
      * @param Customer $object
      */
@@ -232,14 +211,13 @@ class CustomerAdmin extends BaseAdmin
     {
         parent::preUpdate($object);
     }
-    
+
     ///////////////////////////////////
     ///
     ///
     ///
     ///////////////////////////////////
-    
-    
+
     /**
      * {@inheritdoc}
      */
@@ -250,6 +228,4 @@ class CustomerAdmin extends BaseAdmin
             ->add('name');
         parent::configureDatagridFilters($filterMapper);
     }
-    
-    
 }
--- libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Messaging/MessageTemplateAdmin.php
+++ libraries/bundle/MagentaSWarrantyAdminBundle/src/Admin/Messaging/MessageTemplateAdmin.php
@@ -2,243 +2,228 @@
 
 namespace Magenta\Bundle\SWarrantyAdminBundle\Admin\Messaging;
 
-use Doctrine\ORM\EntityRepository;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
-use Magenta\Bundle\SWarrantyAdminBundle\Admin\AccessControl\ACLAdmin;
 use Magenta\Bundle\SWarrantyAdminBundle\Admin\BaseAdmin;
-use Magenta\Bundle\SWarrantyAdminBundle\Admin\Product\ProductAdmin;
-use Magenta\Bundle\SWarrantyAdminBundle\Form\Type\ManyToManyThingType;
-use Magenta\Bundle\SWarrantyAdminBundle\Form\Type\MediaCollectionType;
-use Magenta\Bundle\SWarrantyAdminBundle\Form\Type\ProductDetailType;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Customer\Customer;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Messaging\MessageTemplate;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Media\Media;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Module\CommunicationTemplateModule;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Organisation\Organisation;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Person\Person;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Product\Dealer;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\Product\Product;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\Product\ServiceZone;
-use Magenta\Bundle\SWarrantyModelBundle\Entity\System\SystemModule;
 use Magenta\Bundle\SWarrantyModelBundle\Entity\User\User;
-use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserService;
 use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder;
-use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserManager;
-use Magenta\Bundle\SWarrantyModelBundle\Service\User\UserManagerInterface;
-use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-
-use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
-use Sonata\AdminBundle\Form\Type\ModelType;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
-use Sonata\CoreBundle\Form\Type\DatePickerType;
 use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery;
 use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
-use Sonata\MediaBundle\Form\Type\MediaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class MessageTemplateAdmin extends BaseAdmin {
-	
-	protected $action;
-	
-	protected $datagridValues = array(
-		// display the first page (default = 1)
+class MessageTemplateAdmin extends BaseAdmin
+{
+    protected $action;
+
+    protected $datagridValues = [
+        // display the first page (default = 1)
 //        '_page' => 1,
-		// reverse order (default = 'ASC')
-		'_sort_order' => 'DESC',
-		// name of the ordered field (default = the model's id field, if any)
-		'_sort_by'    => 'id',
-	);
-	
-	protected function filterQueryByOrganisation(ProxyQuery $query, Organisation $organisation) {
-		$pool      = $this->getConfigurationPool();
-		$request   = $this->getRequest();
-		$container = $pool->getContainer();
-		/** @var Expr $expr */
-		$expr = $query->getQueryBuilder()->expr();
-//		$customerAlias = $query->entityJoin([ [ 'fieldName' => 'customer' ] ]);
+        // reverse order (default = 'ASC')
+        '_sort_order' => 'DESC',
+        // name of the ordered field (default = the model's id field, if any)
+        '_sort_by' => 'id',
+    ];
 
-//		return $query->andWhere($expr->eq($customerAlias . '.organisation', $organisation->getId()));
-		return parent::filterQueryByOrganisation($query, $organisation);
-	}
-	
-	public function getNewInstance() {
-		/** @var MessageTemplate $object */
-		$object = parent::getNewInstance();
-		
-		return $object;
-	}
-	
-	public function toString($object) {
-		return $object instanceof MessageTemplate
-			? $object->getName()
-			: 'MessageTemplate'; // shown in the breadcrumb on the create view
-	}
-	
-	protected function getAccess() {
-		return array_merge(parent::getAccess(), [
-		]);
-	}
-	
-	public function isGranted(
-		$name, $object = null
-	) {
-		if(in_array($name, [])) {
-			$org = $this->getCurrentOrganisation();
-			$sys = $org->getSystem();
-			$mod = $sys->getModuleByCode(CommunicationTemplateModule::MODULE_CODE);
-//			return $mod->isUserGranted($this->getCurrentOrganisationMember(), $name, $object, $this->getClass());
-		}
-		
-		return parent::isGranted($name, $object);
-	}
-	
-	public function createQuery($context = 'list') {
-		$request = $this->getRequest();
-		/** @var ProxyQueryInterface $query */
-		$query = parent::createQuery($context);
-		if(empty($this->getParentFieldDescription())) {
+    protected function filterQueryByOrganisation(ProxyQuery $query, Organisation $organisation)
+    {
+        $pool = $this->getConfigurationPool();
+        $request = $this->getRequest();
+        $container = $pool->getContainer();
+        /** @var Expr $expr */
+        $expr = $query->getQueryBuilder()->expr();
+        //		$customerAlias = $query->entityJoin([ [ 'fieldName' => 'customer' ] ]);
+
+        //		return $query->andWhere($expr->eq($customerAlias . '.organisation', $organisation->getId()));
+        return parent::filterQueryByOrganisation($query, $organisation);
+    }
+
+    public function getNewInstance()
+    {
+        /** @var MessageTemplate $object */
+        $object = parent::getNewInstance();
+
+        return $object;
+    }
+
+    public function toString($object)
+    {
+        return $object instanceof MessageTemplate
+            ? $object->getName()
+            : 'MessageTemplate'; // shown in the breadcrumb on the create view
+    }
+
+    protected function getAccess()
+    {
+        return array_merge(parent::getAccess(), [
+        ]);
+    }
+
+    public function isGranted(
+        $name, $object = null
+    ) {
+        if (in_array($name, [])) {
+            $org = $this->getCurrentOrganisation();
+            $sys = $org->getSystem();
+            $mod = $sys->getModuleByCode(CommunicationTemplateModule::MODULE_CODE);
+            //			return $mod->isUserGranted($this->getCurrentOrganisationMember(), $name, $object, $this->getClass());
+        }
+
+        return parent::isGranted($name, $object);
+    }
+
+    public function createQuery($context = 'list')
+    {
+        $request = $this->getRequest();
+        /** @var ProxyQueryInterface $query */
+        $query = parent::createQuery($context);
+        if (empty($this->getParentFieldDescription())) {
 //            $this->filterQueryByPosition($query, 'position', '', '');
-		}
-		/** @var Expr $expr */
-		$expr = $query->expr();
-		/** @var QueryBuilder $qb */
-		$qb        = $query->getQueryBuilder();
-		$rootAlias = $qb->getRootAliases()[0];
-		
-		if( ! empty($org = $this->getCurrentOrganisation(false))) {
-//			$qb->join($customerAlias . '.organisation', 'organisation');
+        }
+        /** @var Expr $expr */
+        $expr = $query->expr();
+        /** @var QueryBuilder $qb */
+        $qb = $query->getQueryBuilder();
+        $rootAlias = $qb->getRootAliases()[0];
+
+        if (!empty($org = $this->getCurrentOrganisation(false))) {
+            //			$qb->join($customerAlias . '.organisation', 'organisation');
 //			$query->andWhere($expr->eq($customerAlias . '.organisation', $org->getId()));
-		}
-		$sql = $qb->getQuery()->getSQL();
-		
-		return $query;
-	}
-	
-	public function getPersistentParameters() {
-		$parameters = parent::getPersistentParameters();
-		if( ! $this->hasRequest()) {
-			return $parameters;
-		}
-		
-		return array_merge($parameters, array(
-			'organisation' => $this->getCurrentOrganisation(false)->getId()
-		));
-	}
-	
-	public function configureRoutes(RouteCollection $collection) {
-		parent::configureRoutes($collection);
-		$collection->add('detail', $this->getRouterIdParameter() . '/detail');
-		$collection->add('transferOwnership', $this->getRouterIdParameter() . '/transfer-ownership/{customerId}');
-	}
-	
-	protected function configureShowFields(ShowMapper $showMapper) {
-		$showMapper
-			->with('form_group.MessageTemplate_details', [ 'class' => 'col-md-6' ])
-			->add('name', null, [ 'label' => 'form.label_name' ])
-			->add('subject', null, [ 'label' => 'form.label_subject' ])
-			->add('content', CKEditorType::class, [ 'label' => 'form.label_content' ])
-			->end();
-	}
-	
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function configureListFields(ListMapper $listMapper) {
-		$listMapper->add('_action', 'actions', [
-				'actions' => array(
-					'edit'   => array(),
-					'delete' => array(),
+        }
+        $sql = $qb->getQuery()->getSQL();
+
+        return $query;
+    }
+
+    public function getPersistentParameters()
+    {
+        $parameters = parent::getPersistentParameters();
+        if (!$this->hasRequest()) {
+            return $parameters;
+        }
+
+        return array_merge($parameters, [
+            'organisation' => $this->getCurrentOrganisation(false)->getId(),
+        ]);
+    }
+
+    public function configureRoutes(RouteCollection $collection)
+    {
+        parent::configureRoutes($collection);
+        $collection->add('detail', $this->getRouterIdParameter().'/detail');
+        $collection->add('transferOwnership', $this->getRouterIdParameter().'/transfer-ownership/{customerId}');
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->with('form_group.MessageTemplate_details', ['class' => 'col-md-6'])
+            ->add('name', null, ['label' => 'form.label_name'])
+            ->add('subject', null, ['label' => 'form.label_subject'])
+            ->add('content', CKEditorType::class, ['label' => 'form.label_content'])
+            ->end();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->add('_action', 'actions', [
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
 //					'send_evoucher' => array( 'template' => '::admin/employer/employee/list__action_send_evoucher.html.twig' )
 
 //                ,
 //                    'view_description' => array('template' => '::admin/product/description.html.twig')
 //                ,
 //                    'view_tos' => array('template' => '::admin/product/tos.html.twig')
-				),
-				'label'   => 'form.label_action'
-			]
-		);
-		
-		$listMapper
-			->add('name', null, [ 'label' => 'form.label_name' ])
-			->add('subject', null, [ 'label' => 'form.label_subject' ])
-			->add('enabled', null, [ 'label' => 'form.label_enabled', 'editable' => true ]);
+                ],
+                'label' => 'form.label_action',
+            ]
+        );
 
-//		$listMapper->add('positions', null, [ 'template' => '::admin/user/list__field_positions.html.twig' ]);
-	}
-	
-	protected function configureFormFields(FormMapper $formMapper) {
-		$c = $this->getConfigurationPool()->getContainer();
-		$formMapper->add('type', ChoiceType::class, array(
-			'multiple'           => false,
-			'placeholder'        => 'Select Template Type',
-			'choices'            => [
-				'form_field.label_registration_email_verification_template' => MessageTemplate::TYPE_REGISTRATION_VERIFICATION,
-				'form_field.label_registration_copy_template'               => MessageTemplate::TYPE_REGISTRATION_COPY,
-				'form_field.label_warranty_approved_notif_template'         => MessageTemplate::TYPE_WARRANTY_APPROVED,
-				'form_field.label_technician_new_assignment_template'         => MessageTemplate::TYPE_TECHNICIAN_NEW_ASSIGNMENT,
-				'form_field.label_warranty_new_registration_template'         => MessageTemplate::TYPE_WARRANTY_NEW_REGISTRATION,
-			],
-			'translation_domain' => $this->translationDomain
-		));
-		$formMapper->add('name', null, [ 'label' => 'form.label_name' ]);
-		$formMapper->add('subject', null, [ 'label' => 'form.label_subject' ]);
-		$formMapper->add('content', CKEditorType::class, [ 'label' => 'form.label_content' ]);
-		
-		$formMapper->end();
-	}
-	
-	/**
-	 * @param MessageTemplate $object
-	 */
-	public function preValidate($object) {
-		parent::preValidate($object);
-		
-	}
-	
-	/**
-	 * @param MessageTemplate $object
-	 */
-	public function prePersist($object) {
-		parent::prePersist($object);
-		if( ! $object->isEnabled()) {
-			$object->setEnabled(true);
-		}
-	}
-	
-	/**
-	 * @param MessageTemplate $object
-	 */
-	public function preUpdate($object) {
-		parent::preUpdate($object);
-	}
-	
-	///////////////////////////////////
-	///
-	///
-	///
-	///////////////////////////////////
-	
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function configureDatagridFilters(DatagridMapper $filterMapper) {
-		$filterMapper
-			->add('id')//			->add('customer.name')//			->add('locked')
-		;
-		parent::configureDatagridFilters($filterMapper);
-	}
-	
-	
+        $listMapper
+            ->add('name', null, ['label' => 'form.label_name'])
+            ->add('subject', null, ['label' => 'form.label_subject'])
+            ->add('enabled', null, ['label' => 'form.label_enabled', 'editable' => true]);
+
+        //		$listMapper->add('positions', null, [ 'template' => '::admin/user/list__field_positions.html.twig' ]);
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $c = $this->getConfigurationPool()->getContainer();
+        $formMapper->add('type', ChoiceType::class, [
+            'multiple' => false,
+            'placeholder' => 'Select Template Type',
+            'choices' => [
+                'form_field.label_registration_email_verification_template' => MessageTemplate::TYPE_REGISTRATION_VERIFICATION,
+                'form_field.label_registration_copy_template' => MessageTemplate::TYPE_REGISTRATION_COPY,
+                'form_field.label_warranty_approved_notif_template' => MessageTemplate::TYPE_WARRANTY_APPROVED,
+                'form_field.label_technician_new_assignment_template' => MessageTemplate::TYPE_TECHNICIAN_NEW_ASSIGNMENT,
+                'form_field.label_warranty_new_registration_template' => MessageTemplate::TYPE_WARRANTY_NEW_REGISTRATION,
+            ],
+            'translation_domain' => $this->translationDomain,
+        ]);
+        $formMapper->add('name', null, ['label' => 'form.label_name']);
+        $formMapper->add('subject', null, ['label' => 'form.label_subject']);
+        $formMapper->add('content', CKEditorType::class, ['label' => 'form.label_content']);
+
+        $formMapper->end();
+    }
+
+    /**
+     * @param MessageTemplate $object
+     */
+    public function preValidate($object)
+    {
+        parent::preValidate($object);
+    }
+
+    /**
+     * @param MessageTemplate $object
+     */
+    public function prePersist($object)
+    {
+        parent::prePersist($object);
+        if (!$object->isEnabled()) {
+            $object->setEnabled(true);
+        }
+    }
+
+    /**
+     * @param MessageTemplate $object
+     */
+    public function preUpdate($object)
+    {
+        parent::preUpdate($object);
+    }
+
+    ///////////////////////////////////
+    ///
+    ///
+    ///
+    ///////////////////////////////////
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureDatagridFilters(DatagridMapper $filterMapper)
+    {
+        $filterMapper
+            ->add('id')//			->add('customer.name')//			->add('locked')
+        ;
+        parent::configureDatagridFilters($filterMapper);
+    }
 }
